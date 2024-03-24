0db80546a4aedaf9c3785a3187f8d6144c1b8171
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2024-03-24 18:13:13 -0500

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+export function App() {
+  return (
+    <>
+      <h1>EduQuest</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus ipsum
+        dolorem aspernatur eum possimus cupiditate iste quos impedit perferendis
+      </p>
+    </>
+  );
+}
