49241a538f835a5be5b50edff4ae97f2938ae606
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2024-03-24 21:00:06 -0500

    feat 🎉 implement main component

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,5 +1,30 @@
+import { useRef } from "react"
+import { Link } from "react-router-dom"
+
 export const Main = () => {
+
+  const inputRef = useRef(null)
+
   return (
-    <div>Root Element</div>
+    <div>
+      <h1>Quiz Aplication</h1>
+
+      <ol>
+        <li>You will be asked 10 questions one after another.</li>
+        <li>10 points is awarded for the correct answer.</li>
+        <li>Each question has three options. You can choose only one options.</li>
+        <li>You can review and change answers before the quiz finish.</li>
+        <li>The result will be declared at the end of the quiz.</li>
+      </ol>
+
+      <form id="form">
+        <input ref={inputRef} type="text" placeholder="Username*" />
+      </form>
+
+      <div>
+        <Link to={"quiz"}>Start Quiz</Link>
+      </div>
+
+    </div>
   )
 }
