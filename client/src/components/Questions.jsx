bbda695ea501c22cb37ddd8de9af9cf28bc4152a
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2024-03-25 09:55:38 -0500

    add data for questions

--- client/src/components/Questions.jsx
+++ client/src/components/Questions.jsx
@@ -1,7 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import data from "../database/data";
 
 export const Questions = () => {
   const [checked, setChecked] = useState(false);
+
+  const question = data[0]
+
+  useEffect(() => {
+    console.log(data)
+  })
 
   function onSelected() {
     setChecked(true);
@@ -10,20 +17,34 @@ export const Questions = () => {
 
   return (
     <div className="p-4 bg-white rounded-lg shadow-md">
-      <h2 className="text-xl font-bold mb-4">Simple Question 1</h2>
+      <h2 className="text-xl font-bold mb-4">{question.question}</h2>
 
-      <ul className="list-none">
-        <li className="flex items-center mb-2">
-          <input
-            id="q1-option"
-            type="radio"
-            name="options"
-            checked={checked}
-            onChange={onSelected}
-            className="mr-2 h-5 w-5 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-          />
-          <label htmlFor="q1-option" className="text-gray-700">Option</label>
-        </li>
+      <ul
+        className="list-none"
+        key={question.id}
+      >
+        {
+          question.options.map((question, index) => (
+            <li
+              className="flex items-center mb-2"
+              key={index}
+            >
+              <input
+                id={`q${index}`}
+                type="radio"
+                name="options"
+                checked={checked}
+                onChange={onSelected}
+                className="mr-2 h-5 w-5 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+              />
+              <label
+                htmlFor={`q${index}-option`}
+                className="text-gray-700">
+                {question}
+              </label>
+            </li>
+          ))
+        }
       </ul>
     </div>
   );
