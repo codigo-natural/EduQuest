9b2aa17329c5042a027e4ea3e091f8759db32828
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2024-04-01 08:32:21 -0500

    feat 🎉 establish connection with the database

--- server/server.js
+++ server/server.js
@@ -1,23 +1,24 @@
+import { config } from "dotenv";
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
-import { config } from "dotenv";
 import router from "./router/router.js";
+import connectDB from "./database/conn.js";
 
 const app = express();
+config();
 
 // app middlewares
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
-config();
 
 // application port
 const PORT = process.env.PORT || 8080;
 
 // routes
 
-app.use("/api", router) // apis
+app.use("/api", router); // apis
 
 app.get("/", (req, res) => {
   try {
@@ -27,6 +28,17 @@ app.get("/", (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`server connected to http://localhost:${PORT}`);
-});
+// start server only when we have valid connection
+connectDB()
+  .then(() => {
+    try {
+      app.listen(PORT, () => {
+        console.log(`server connected to http://localhost:${PORT}`);
+      });
+    } catch (error) {
+      console.log("Cannot connect to the server");
+    }
+  })
+  .catch((error) => {
+    console.log("Invalid database connection");
+  });
